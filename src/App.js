c5589cadf9e1a3ca29697a0f419fd3613819f5c7
oliver balassa <balassa.oliver@outlook.com>
2023-10-10 18:30:12 +0200

    snapshot 10.10.
    18:30

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button, Container, Card, Row, Col } from 'react-bootstrap'
-import PrintQuestionModel from './logic/QuizQuestion'
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import LandingPage from "./components/LandingPage"
 
-function App () {
-  return (
-    <Container
-      className='d-flex justify-content-center align-items-center'
-      style={{ minHeight: '100vh' }}
-    >
-      <Card className='shadow-lg' id='Card'>
-        <Card.Body>
-          <Row>
-            <Col className='d-flex justify-content-center'>
-              <Card.Title class='Font'>Gyere Játsz velünk Kvízt </Card.Title>
-            </Col>
-          </Row>
-          <Row xs={2}>
-            <Col md />
-            <Col className='d-flex justify-content-center' md>
-              <Button variant='outline-success' onClick={PrintQuestionModel}>
-                Start
-              </Button>
-            </Col>
-            <Col md />
-          </Row>
-        </Card.Body>
-      </Card>
-    </Container>
-  )
+function App() {
+    return (
+        <LandingPage/>
+    );
 }
 
-export default App
+export default App;
