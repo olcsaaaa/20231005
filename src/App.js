f743e29eb1688ec4c2a99f0aaa4888470258a485
oliver balassa <balassa.oliver@outlook.com>
2023-10-10 14:39:30 +0200

    created KEPZESI branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Button,  Container, Card, Row, Col} from "react-bootstrap";
+import PrintQuestionModel from "./logic/QuizQuestion";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    return (
+        <Container className="d-flex justify-content-center align-items-center" style={{minHeight: '100vh'}}>
+            <Card className="shadow-lg" id={"Card"} >
+                <Card.Body>
+
+                    <Row>
+                    <Col  className="d-flex justify-content-center">
+                    <Card.Title class={"Font"}>Gyere Játsz velünk Kvízt </Card.Title>
+                    </Col>
+                    </Row>
+                    <Row>
+                    <Col className="d-flex justify-content-center">
+                        <Button variant='outline-success' onClick={PrintQuestionModel}>Start</Button>
+                    </Col>
+                    </Row>
+                </Card.Body>
+            </Card>
+        </Container>
+    );
 }
 
 export default App;
---
+++ src/logic/QuizQuestion.js
@@ -0,0 +1,21 @@
+// const questionMatrix = [
+//     ["Question", "Answer1", "Answer2", "Answer3", "Answer4", "Index of correct answer(int)"],
+//     ["Q1", "A1", "B1", "C1","D1",2],
+//     ["Q2", "A2", "B2", "C2","D2",4],
+//     ["Q3", "A3", "B3", "C3","D3",3],
+// ]
+const modelQuestions = [
+    {Question: "q1",Answer1: "a1", Answer2: "b2", Answer3: "sany",Truth: "42"},
+    {Question: "",Answer1: "", Answer2: "", Answer3: "",Truth: ""},
+    {Question: "",Answer1: "", Answer2: "", Answer3: "",Truth: ""},
+    {Question: "",Answer1: "", Answer2: "", Answer3: "",Truth: ""},
+]
+
+const PrintQuestionModel = function (index) {
+ const model = modelQuestions[0]
+    const message = "Question:"+model.Question+", A1: "+model.Answer1
+    prompt(message)
+
+
+}
+export default PrintQuestionModel
