17cbc66d040c19756a1a2262325d7e9ee1f35b30
oliver balassa <balassa.oliver@outlook.com>
2023-10-12 15:00:09 +0200

    snapshot 12:59 10.12.23

--- src/logic/setQuestion.js
+++ src/logic/setQuestion.js
@@ -1,6 +1,35 @@
-import GetQnA from './GetQnA'
+import GetQnA from "./GetQnA";
 class SetQuestion {
-  maxQuestions = 5
-  questionsAnswered = 0
-  score = 0
+    maxQuestions = 5
+    questionsSubmitted = 0
+    score = 0
+    allQuestionIndexes = GetQnA.getQuestionIndexes
+    gotQuestions = []
+
+    setText(id, text) {
+        const element = document.getElementById(id)
+        element.innerText = text
+    }
+
+    setQuestion(elementId, questionId) {
+        const qtext = GetQnA.getQuestion(questionId)
+        this.setText(elementId, qtext)
+    }
+
+    setAnswer(elementId, questionId, answerId) {
+        const atext = GetQnA.getAnswer(questionId, answerId)
+        this.setText(elementId, atext)
+    }
+
+    nextQuestion() {
+        if (this.questionsSubmitted === 0) {
+            let qId = Math.random(0,this.allQuestionIndexes)
+            this.gotQuestions.push(qId)
+            this.questionsSubmitted++
+
+        }
+    }
+
 }
+
+export default new SetQuestion()
